5118fe5383d0e3275c2de1d57daeddc369aa1dfb
Arian Haliti <arian93haliti@gmail.com>
2019-10-20 19:51:59 +0200

    Added Search bar with logs,logitem and preloader

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useEffect, Fragment } from "react";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+import "./App.css";
+import SearchBar from "./components/layout/SearchBar";
+import Logs from "./components/logs/Logs";
+const App = () => {
+  useEffect(() => {
+    // Init Materialize JS
+    M.AutoInit();
+  });
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
+    </Fragment>
+  );
+};
+
+export default App;
